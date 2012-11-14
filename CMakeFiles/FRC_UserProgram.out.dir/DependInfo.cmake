447bb035539c906fe0f4401a6f54b09fa6c038b9
Tyler Veness <calcmogul@gmail.com>
2012-11-13 17:52:11 -0800

    Fixed Thermopylae to build with new build system

---
+++ CMakeFiles/FRC_UserProgram.out.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tav/Git/Thermopylae/Autonomous.cpp" "/home/tav/Git/Thermopylae/CMakeFiles/FRC_UserProgram.out.dir/Autonomous.cpp.obj"
+  "/home/tav/Git/Thermopylae/Disabled.cpp" "/home/tav/Git/Thermopylae/CMakeFiles/FRC_UserProgram.out.dir/Disabled.cpp.obj"
+  "/home/tav/Git/Thermopylae/OperatorControl.cpp" "/home/tav/Git/Thermopylae/CMakeFiles/FRC_UserProgram.out.dir/OperatorControl.cpp.obj"
+  "/home/tav/Git/Thermopylae/OurRobot.cpp" "/home/tav/Git/Thermopylae/CMakeFiles/FRC_UserProgram.out.dir/OurRobot.cpp.obj"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/powerpc-wrs-vxworks/wind_base/target/h/WPILib"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
